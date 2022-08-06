d1d93546f038f44cd5063d600590a0fe528468e2
Laxmikant-Kulkarni <lvkulkarni2000@gmail.com>
2022-08-06 09:04:14 +0200

    commititing selenium mvn setup for Intellij Idea

---
+++ src/test/java/ChromeLaunch.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeLaunch {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","D:\\Workspace\\SDET_Intellij\\SeleniumMVNSetup\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.quit();
+    }
+}
